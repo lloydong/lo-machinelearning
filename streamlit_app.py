d503eecc94ea4135b5def4a1fe9924f4861d9aea
Lloyd Ong <lloyd_ong@hotmail.com>
2025-01-22 13:29:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ with st.expander('Data'):
 
   st.write('**y**')
   y = df.count
+  y
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='temperature',y='count', x_label='Temperature,°C', y_label='Total Rentals')

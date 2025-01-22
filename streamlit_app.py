375924340dfaeb0b0242a7ef633b6e01b51316b0
Lloyd Ong <lloyd_ong@hotmail.com>
2025-01-22 09:29:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🤖Machine Learning App')
 st.info('This app builds a machine learning model!')
 
 with st.expander('Data'):
-  st.write('**Raw data**)
+  st.write('**Raw data**')
   df = pd.csv('https://raw.githubusercontent.com/lloydong/lo-machinelearning/refs/heads/master/data/bikeshare.csv')
   df

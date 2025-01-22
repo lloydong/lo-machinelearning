ed68605e40ec181bbb90c4d01ba88423a25743b7
Lloyd Ong <lloyd_ong@hotmail.com>
2025-01-22 10:33:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,11 @@ with st.sidebar:
   input_df=pd.DataFrame(data, index=[0])
   input_rentals = pd.concat([input_df,X],axis=0)
 
-input_df
+with st.expander('Input features'):
+  st.write('**Input rental**')
+  input_df
+  st.write('**Combined rentals data**')
+  input_rentals
 
 
 
